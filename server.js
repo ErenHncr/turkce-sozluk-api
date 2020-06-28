952c5c9022ed31a72814570d9e45a6a3d36c6861
erenhncr <erenhncr@gmail.com>
2020-06-28 15:39:53 +0300

    2 endpoint added

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const { tdkSearch, tdkIcerik } = require('./tdk');
 
 app.use(cors());
-app.use(bodyParser.json());
 
-app.get('/', (req, res) => tdkSearch(req, res));
+app.get('/search', (req, res) => tdkSearch(req, res));
+app.get('/icerik', (req, res) => tdkIcerik(req, res));
+app.get('/*', (req, res) => res.json('Please use /search or /icerik endpoints.'));
 
 app.listen(3000, () => {
   console.log('app is running on port 3000');
