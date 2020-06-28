38378cf2119245dfcbb4f0917a031ca2e272b71d
erenhncr <erenhncr@gmail.com>
2020-06-28 16:16:35 +0300

    oneri endpoint added

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { tdkSearch, tdkIcerik } = require('./tdk');
+const { tdkSearch, tdkIcerik, tdkKelimeOneri } = require('./tdk');
 
 app.use(cors());
 
 app.get('/search', (req, res) => tdkSearch(req, res));
 app.get('/icerik', (req, res) => tdkIcerik(req, res));
+app.get('/oneri', (req, res) => tdkKelimeOneri(req, res));
 app.get('/*', (req, res) => res.json('Please use /search or /icerik endpoints.'));
 
 app.listen(3000, () => {
